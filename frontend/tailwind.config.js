e4918bcc64743b0b2382cdd4c114ad69711725aa
pattharock <ritvik1609#gmail.com>
2021-02-13 19:26:18 +0800

    changed background on HomePage.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {
-        colors:{
-          schemeColor: '#FF7F41',
-        },
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        schemeColor: "#1a202c",
       },
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
