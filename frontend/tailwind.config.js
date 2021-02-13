458f07bc495bf2e3f5ef8340057ac98b9189f37b
pattharock <ritvik1609#gmail.com>
2021-02-13 19:38:57 +0800

    merge conflicts tailwind.config.js resolved

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,10 @@
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {
-        colors:{
-          schemeColor: '#FF7F41',
-        },
-        minWidth:{
-          '1/3':'33%'
-        }
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        schemeColor: "#1a202c",
       },
     },
   },
