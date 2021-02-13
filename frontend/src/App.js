2f96b47b387809f098245c75c920d22a6eb8ba9f
raunakchopra <raunakchopra@github.com>
2021-02-13 23:12:48 +0800

    New

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,16 @@
 import React from "react";
-<<<<<<< HEAD
-import HomePage from './screens/HomePage'
-import FeatureDisplay from './screens/FeatureDisplay'
-import CuratedEvent from './screens/CuratedEvent'
-import ProfileGenForm from './screens/ProfileGenForm'
-=======
 import HomePage from "./screens/HomePage";
 import FeatureDisplay from "./screens/FeatureDisplay";
 import CuratedEvent from "./screens/CuratedEvent";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
->>>>>>> ad0f9f1ae5f3cfc64093d3644928fa52423a82b2
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <ProfileGenForm />
-=======
       <Header />
       <HomePage />
       <Footer />
->>>>>>> ad0f9f1ae5f3cfc64093d3644928fa52423a82b2
     </div>
   );
 }
