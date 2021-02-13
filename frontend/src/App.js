4fcf1ce36d9180152628286bf18f4fe4954ac572
raunakchopra <raunakchopra@github.com>
2021-02-13 16:20:48 +0800

    Home Page Screen init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import HomePage from './screens/HomePage'
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <HomePage />
     </div>
   );
 }
---
+++ frontend/src/screens/HomePage.js
@@ -0,0 +1,32 @@
+import React,{useState} from 'react'
+
+const HomePage = () => {
+    const [showModal, setShowModal] = useState(false)
+    return (
+        <div className="flex flex-col w-screen mt-8">
+            {/* <div className="w-full justify-between flex">
+                <div className="px-6 py-2 bg-schemeColor">
+                    <h1 className="text-white font-bold">Logo</h1>
+                </div>
+                <div>
+                    <button onClick={() => setShowModal(true)} className="outline-none text-white py-2 px-6 rounded-xl bg-schemeColor font-bold text-xl">Login</button>
+                </div>
+            </div> */}
+            <div className="bg-schemeColor w-1/3 h-full rounded-3xl self-center py-10 px-12">
+                <h1 className="text-white font-bold mb-8 text-center font-sans text-3xl">Get Started</h1>
+                <h3 className="font-bold text-white mb-8 text-xl text-center mb-2"> By Clicking Log In, you agree to our terms. Learn how we proecess your data in our Privacy Policy and Cookie Policy</h3>
+                <div className="flex flex-col text-white items-center">
+                    <button className="border-white border-4 w-2/3 py-2 text-white rounded-full text-xl font-bold mb-4"><div className="flex pl-8"><img src="https://img.favpng.com/15/12/25/google-logo-google-adwords-g-suite-google-account-png-favpng-ZPDpvjf5PW8XaAnw6V9PQkcut.jpg" className="h-12 w-12 rounded-full" alt="google" /> <p className="ml-4 self-center">LOG IN WITH GOOGLE</p> </div></button>
+                    <button className="border-white border-4 w-2/3 py-2 text-white rounded-full text-xl font-bold mb-4"><div className="flex pl-8"><img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" className="h-12 w-12 rounded-full" alt="google" /> <p className="ml-4 self-center">LOG IN WITH FACEBOOK</p> </div></button>
+                    <h1 className="font-bold text-lg"> Trouble Logging In?</h1>            
+                </div>
+            </div>
+            <div className="w-full">
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default HomePage
