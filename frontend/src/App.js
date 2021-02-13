545575d9e5f56cb864c54d4b6ef4419e0721c92d
raunakchopra <raunakchopra@github.com>
2021-02-13 19:26:18 +0800

    Curated Events added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import HomePage from './screens/HomePage'
 import FeatureDisplay from './screens/FeatureDisplay'
+import CuratedEvent from './screens/CuratedEvent'
 
 function App() {
   return (
     <div>
-      <FeatureDisplay />
+      <CuratedEvent />
     </div>
   );
 }
