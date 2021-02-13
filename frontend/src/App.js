ad0f9f1ae5f3cfc64093d3644928fa52423a82b2
pattharock <ritvik1609#gmail.com>
2021-02-13 23:07:39 +0800

    react bootsrap refactor 1.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import HomePage from './screens/HomePage'
-import FeatureDisplay from './screens/FeatureDisplay'
-import CuratedEvent from './screens/CuratedEvent'
+import HomePage from "./screens/HomePage";
+import FeatureDisplay from "./screens/FeatureDisplay";
+import CuratedEvent from "./screens/CuratedEvent";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
     <div>
-      <CuratedEvent />
+      <Header />
+      <HomePage />
+      <Footer />
     </div>
   );
 }
